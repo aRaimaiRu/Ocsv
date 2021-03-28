a1d3c9d749eff17dd6e4e94eccc388f6fe434663
22p26c0377 <pawut.j@gmail.com>
2021-03-28 18:53:42 +0800

    Change ContentSection to State

--- src/component/contentsection.js
+++ src/component/contentsection.js
@@ -1,52 +1,50 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ChoicePage from "./containers/ChoicePage";
-export default class contentsection extends Component {
-  render() {
-    return (
-      <div className="container" style={{ borderStyle: "solid" }}>
-        <form
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
+export default function ContentSection() {
+  return (
+    <div className="container" style={{ borderStyle: "solid" }}>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <input
+          type="text"
+          style={{ width: "20em", margin: "auto" }}
+          placeholder="content"
+        ></input>
+        <textarea
+          style={{ width: "100%" }}
+          placeholder="html editor"
+        ></textarea>
+        <div>Create</div>
+        <ChoicePage />
+        <row className="mgt">
+          <label style={{ float: "left", marginBottom: "0" }}>
+            อธิบายเฉลยเพิ่มเติม
+          </label>
           <input
+            class="text-input "
             type="text"
-            style={{ width: "20em", margin: "auto" }}
-            placeholder="content"
-          ></input>
-          <textarea
-            style={{ width: "100%" }}
-            placeholder="html editor"
-          ></textarea>
-          <div>Create</div>
-          <ChoicePage />
-          <row className="mgt">
-            <label style={{ float: "left", marginBottom: "0" }}>
-              อธิบายเฉลยเพิ่มเติม
-            </label>
-            <input
-              class="text-input "
-              type="text"
-              name="Explain"
-              id="Explain"
-              style={{ float: "left" }}
-            />
-          </row>
-          <row className="mgt">
-            <label style={{ float: "left", marginBottom: "0" }}>
-              Link เมืองนอก
-            </label>
-            <input
-              class="text-input "
-              type="text"
-              name="AditionalLink"
-              id="AditionalLink"
-              style={{ float: "left" }}
-            />
-          </row>
-        </form>
-      </div>
-    );
-  }
+            name="Explain"
+            id="Explain"
+            style={{ float: "left" }}
+          />
+        </row>
+        <row className="mgt">
+          <label style={{ float: "left", marginBottom: "0" }}>
+            Link เมืองนอก
+          </label>
+          <input
+            class="text-input "
+            type="text"
+            name="AditionalLink"
+            id="AditionalLink"
+            style={{ float: "left" }}
+          />
+        </row>
+      </form>
+    </div>
+  );
 }
