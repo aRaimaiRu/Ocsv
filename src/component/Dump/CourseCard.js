1d47aaacd93bff8cbcef60229e1633bb6e37c2c2
Kittipat Poosrimoung <first1801@hotmail.com>
2021-04-20 15:46:43 +0700

    function create

--- src/component/Dump/CourseCard.js
+++ src/component/Dump/CourseCard.js
@@ -1,10 +1,17 @@
 import React from "react";
 import MyCard from "./outlinedCard";
-export default function CourseCard({ courses }) {
+export default function CourseCard({ courses, token }) {
+  console.log(test);
   return (
     <div className="Courses">
       {Object.values(courses).map((obj) => (
-        <MyCard grade={obj.grade} module={obj.module} id={obj.id} />
+        <MyCard
+          // grade={obj.grade}
+          // module={obj.module}
+          // id={obj.id}
+          obj={obj}
+          token={token}
+        />
       ))}
     </div>
   );
